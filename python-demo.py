27ef88bc3ed01e2d8174830f9fb1b9a9b4614144
Akwa <akwaking@gmail.com>
2024-11-03 22:12:43 +0100

    Add files via upload

---
+++ python-demo.py
@@ -0,0 +1,56 @@
+#value = 0
+
+#if value == 0:
+    #print("low")
+
+
+    ##data type#
+#string
+#bool
+#float
+#intergers
+#None
+
+# account_name = "akwa"
+# account_number = "674653817"
+# secret_code = "1234"
+# account_balance = 10000
+
+#operators
+#arithmetic oprator should depend on the use case
+#+,-,/
+# A = 1000 + account_balance
+
+### identity operatr
+
+#movie1 = ["spiderman, "hulk", "avenger"]
+#movie2 = ["spiderman", "hulk", "avenger"]
+#movie3 = ["the one", "the game", "omen"]
+#
+#
+#Gender1 = "male"
+#Gender2 = "male"
+#Gender3 = "female"
+#print(Gender1 is Gender2)
+#print(Gender1  is not Gender3)
+
+
+#comparison operator
+
+# account_name = "akwa"
+# account_number = "674653817"
+# secret_code = "1234"
+# account_balance = 10000
+
+#pass_account_number 
+#pass_account_number = "674653817"
+#if pass_account_number == account_number:
+ #   print(f"your account balance: {account_balance}CFA France")
+
+
+ #logical operator
+#  passport = True
+#  visa = True
+#  Admission_letter = False
+
+# print(passport and visa)
